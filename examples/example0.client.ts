b0d08b3a6eaeeed8e31657ac807bb0e679983186
Tom Aylott <oblivious@subtlegradient.com>
2024-02-18 17:29:39 -0500

    Refactor example0.client.ts to separate server-side code into a separate function

--- examples/example0.client.ts
+++ examples/example0.client.ts
@@ -1,8 +1,23 @@
 /// <reference lib="dom" />
 "use client"
 
+export {} // Make this a module
+
 console.assert(typeof window !== "undefined", "This is client-side code, so `window` should be defined.")
 
-export default function example0() {
+async function doStuffOnTheServer() {
+  "use server"
+  console.assert(typeof window === "undefined", "This is server-side code, so `window` should not be defined.")
+
+  const { $ } = await import("bun")
+  return await $`ls -la`.text()
+}
+
+export async function example() {
+  // "use client" is not necessary here, because it's already at the top of the file
+  console.assert(typeof window !== "undefined", "This is client-side code, so `window` should be defined.")
   console.log("Hello from example0!")
+  const results = await doStuffOnTheServer()
+  console.log("Server stuff done!")
+  console.log(results)
 }
