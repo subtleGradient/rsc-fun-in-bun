345e4403679d4d6303b46d301b0cc831fd8168ef
Tom Aylott <oblivious@subtlegradient.com>
2024-02-18 17:33:36 -0500

    Refactor doStuffOnTheServer function and update function call

--- examples/example0.client.ts
+++ examples/example0.client.ts
@@ -5,7 +5,7 @@ export {} // Make this a module
 
 console.assert(typeof window !== "undefined", "This is client-side code, so `window` should be defined.")
 
-async function doStuffOnTheServer() {
+export async function doStuffOnTheServer_definedInClientCode() {
   "use server"
   console.assert(typeof window === "undefined", "This is server-side code, so `window` should not be defined.")
 
@@ -17,7 +17,7 @@ export async function example() {
   // "use client" is not necessary here, because it's already at the top of the file
   console.assert(typeof window !== "undefined", "This is client-side code, so `window` should be defined.")
   console.log("Hello from example0!")
-  const results = await doStuffOnTheServer()
+  const results = await doStuffOnTheServer_definedInClientCode()
   console.log("Server stuff done!")
   console.log(results)
 }
