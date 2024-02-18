a109d1dc988be99b7d74641cc9f93f74eba175e9
Tom Aylott <oblivious@subtlegradient.com>
2024-02-18 17:09:54 -0500

    use client example0

---
+++ examples/example0.client.ts
@@ -0,0 +1,8 @@
+/// <reference lib="dom" />
+"use client"
+
+console.assert(typeof window !== "undefined", "This is client-side code, so `window` should be defined.")
+
+export default function example0() {
+  console.log("Hello from example0!")
+}
