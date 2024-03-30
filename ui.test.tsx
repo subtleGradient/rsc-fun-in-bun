28d34dcf26c812e27dda2224a88d8ec988079c34
Tom Aylott <oblivious@subtlegradient.com>
2024-03-30 00:23:47 -0400

    Update UI test and client components

--- ui.test.tsx
+++ ui.test.tsx
@@ -1,11 +1,21 @@
 import { type Server } from "bun"
 import { afterAll, beforeAll, describe, expect, it } from "bun:test"
-import { html } from "./examples/js"
+import { html, js } from "./examples/js"
 import { browser } from "./test-helpers/puppers"
 import { arrayToStream } from "./util/arrayToStream"
 import { concatStreams } from "./util/compoReadableStream"
 
-/** this is probably stupid 🤷‍♂️ */
+if (!1!) {
+  describe("the `await using` keyword", async () => {
+    it("disposes of the page auto-magically", async () => {
+      await using page = Object.assign(await browser.newPage(), { [Symbol.asyncDispose]: () => page.close() })
+      await page.goto("about:blank")
+      // no need to call page.close() here! 🤯
+    })
+  })
+}
+
+/** this is probably bad for reasons 🤷‍♂️ */
 const Port = {
   PORT: 3000,
   get next() {
@@ -92,28 +102,113 @@ describe("HTML server", () => {
   })
 })
 
-describe.todo("SSR", () => { })
-describe.todo("with static server components", () => { })
-describe.todo("with async server components", () => { })
-describe.todo("with client components", () => { })
-describe.todo("hydrated", () => { })
+describe.todo("server components", () => { })
+describe.todo("async server components", () => { })
 
-describe("pupItUp 1", async () => {
-  it("works", async () => {
-    using page = Object.assign(await browser.newPage(), { [Symbol.dispose]: () => page.close() })
-    await page.goto("about:blank")
-    // const text = await page.evaluate(() => document.body.textContent)
-    // expect(text).toBe("Hello from RSC!")
-    // await Bun.sleep(1000)
+describe("client first (classic)", () => {
+  async function htmlStream() {
+    const ReactDOMServer = await import("react-dom/server")
+    return concatStreams(
+      arrayToStream(
+        html`<!DOCTYPE html><HTML>`,
+        ReactDOMServer.renderToString(
+          <head>
+            <title>Hello from ReactDOMServer.renderToString!</title>
+          </head>
+        ),
+        html`<SCRIPT>${js`
+          // fake commonjs environment for react-jsx-dev-runtime
+          window.process = { env: { NODE_ENV: "development" } }
+          window.require = (module) => {
+            if (module === "react") return window.React
+            if (module === "react-dom") return window.ReactDOM
+            throw new Error("Unknown module: " + module)
+          }
+          window.exports = window
+          `}</SCRIPT>`,
+        html`<BODY>`,
+      ),
+      await ReactDOMServer.renderToReadableStream(
+        <>
+          <h1>Hello from ReactDOMServer.renderToReadableStream!</h1>
+          <div id="root" />
+          <script src="/node_modules/react/umd/react.development.js" />
+          <script src="/node_modules/react-dom/umd/react-dom.development.js" />
+          {/* <script src="/node_modules/react-dom/umd/react-dom-server.browser.development.js" /> */}
+          {/* <script src="/node_modules/react-server-dom-webpack/umd/react-server-dom-webpack-server.browser.development.js" /> */}
+          {/* <script src="/node_modules/react-server-dom-webpack/umd/react-server-dom-webpack-client.browser.development.js" /> */}
+          {/* <script src="https://unpkg.com/babel-standalone@6/babel.js" /> */}
+          <script src="/node_modules/react/cjs/react-jsx-dev-runtime.development.js" />
+        </>,
+      ),
+      arrayToStream(
+        html`<div id="root">loading...</div>`,
+        html`<script>${js`          
+          ${function main() {
+            const { React, ReactDOM } = window as any as { React: typeof import("react"), ReactDOM: typeof import("react-dom/client") }
+            console.log(React.version)
+
+            function ClientComponent() {
+              return <div id="generated-by-client">Hello from ClientComponent!</div>
+            }
+
+            const root = ReactDOM.hydrateRoot(document.getElementById("root")!, <ClientComponent />)
+            // root.render(<ClientComponent />)
+          }}
+          main()
+          `.trim()}</script>`,
+      ),
+    )
+  }
+
+  let server: Server
+  beforeAll(() => {
+    server = Bun.serve({
+      port: Port.next,
+      async fetch(request) {
+        const url = new URL(request.url)
+
+        if (url.pathname === "/") {
+          return new Response(await htmlStream(), { headers: { "Content-Type": "text/html" } })
+        }
+
+        const file = Bun.file(__dirname + url.pathname)
+
+        if (await file.exists()) {
+
+          return new Response(file, { headers: { "Content-Type": file.type } })
+          // return new Response((await Bun.build({ target: 'browser', entrypoints: [file.name!], format: 'esm' })).outputs[0], { headers: { "Content-Type": file.type } })
+        }
+
+        return new Response('404 Not Found', { status: 404 })
+      },
+    })
+  })
+  afterAll(() => {
+    server?.stop()
+  })
+
+  describe("client components", () => {
+    describe("render stuff on the client like it's 2014", () => {
+      it("renders a component on the client (using fetch)", async () => {
+        const response = await fetch(server.url.href)
+        expect(response.status).toBe(200)
+        // console.log(await response.text())
+        expect(await response.text()).toMatchSnapshot()
+      })
+
+      it("renders a component on the client (using puppeteer)", async () => {
+        await using page = await browser.newPage()
+        page.pipeConsoleLogs = true
+
+        await page.goto(server.url.href)
+        const version = await page.waitForFunction(() => window?.React?.version).then((x) => x.jsonValue())
+        expect(version).toStartWith("19.")
+        await page.waitForSelector("#generated-by-client")
+      })
+    })
   })
 })
 
-describe("pupItUp 2", async () => {
-  it("works", async () => {
-    using page = Object.assign(await browser.newPage(), { [Symbol.dispose]: () => page.close() })
-    await page.goto("about:blank")
-    // const text = await page.evaluate(() => document.body.textContent)
-    // expect(text).toBe("Hello from RSC!")
-    // await Bun.sleep(1000)
-  })
-})
+describe.todo("server actions", () => { })
+
