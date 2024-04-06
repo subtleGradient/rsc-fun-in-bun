6cc44143383659a8b37dedd2e9838384918d0f52
Tom Aylott <oblivious@subtlegradient.com>
2024-04-06 17:52:34 -0400

    ServerComponentsPlugin ++

--- plugins/ServerComponentsPlugin.ts
+++ plugins/ServerComponentsPlugin.ts
@@ -1,6 +1,43 @@
-import { FileSystemRouter, build, type BuildArtifact, type BunPlugin } from "bun"
-import ServerComponentsPlugin from "bun-plugin-server-components"
-if (!1!) console.log(ServerComponentsPlugin)
+import { FileSystemRouter, type BuildArtifact, type BunPlugin } from "bun"
+
+/**
+ * bun-plugin-server-components@0.0.1-alpha.0, this doesn't work with bun@1.1.2 innards
+ * {@link ServerComponentsPlugin_notWorkingSoGreat}
+ */
+import ServerComponentsPlugin_notWorkingSoGreat from "bun-plugin-server-components"
+
+export function ServerComponentsPlugin(config: { server: string[]; client: string[]; router: FileSystemRouter }): BunPlugin {
+  return {
+    name: "bun (RSC) React Server Components (Bun internals)",
+    SECRET_SERVER_COMPONENTS_INTERNALS: {
+      router: config.router,
+      directive: {
+        client: config.client,
+        server: config.server,
+      },
+    },
+    setup(build) {}, // this is a no-op?
+  }
+}
+
+declare module "bun" {
+  interface BunPlugin {
+    /**
+     * types for {@link BunPlugin.SECRET_SERVER_COMPONENTS_INTERNALS} of bun@1.1.2
+     *
+     * Source: https://github.com/oven-sh/bun/blob/main/src/bun.js/api/JSBundler.zig
+     * Permalink: https://github.com/oven-sh/bun/blob/b24d3ba5be361b909928826c112789a8bf70bf4b/src/bun.js/api/JSBundler.zig#L94-L137
+     * More info: https://github.com/oven-sh/bun/issues/3997#issuecomment-1714207143
+     */
+    SECRET_SERVER_COMPONENTS_INTERNALS?: {
+      router?: FileSystemRouter
+      directive: {
+        client: string[]
+        server: string[]
+      }
+    }
+  }
+}
 
 // @ts-expect-error -- missing types
 import { ByteBuffer } from "peechy/bb"
@@ -15,20 +52,6 @@ async function parseComponentsManifest(manifest?: Blob) {
 interface RSCBuildArtifact extends Omit<BuildArtifact, "kind"> {
   kind: BuildArtifact["kind"] | "entry-point" | "component-manifest" | "use client" | "use server"
 }
-declare module "bun" {
-  interface BunPlugin {
-    // https://github.com/oven-sh/bun/issues/3997#issuecomment-1714207143
-    // https://github.com/oven-sh/bun/blob/main/src/bun.js/api/JSBundler.zig
-    // https://github.com/oven-sh/bun/blob/b24d3ba5be361b909928826c112789a8bf70bf4b/src/bun.js/api/JSBundler.zig#L94-L137
-    SECRET_SERVER_COMPONENTS_INTERNALS?: {
-      router?: FileSystemRouter
-      directive: {
-        client: [""]
-        server: [""]
-      }
-    }
-  }
-}
 
 if (import.meta.main) {
   const router = new FileSystemRouter({
@@ -36,19 +59,13 @@ if (import.meta.main) {
     style: "nextjs",
   })
 
-  const serverComponentsPlugin: BunPlugin = {
-    setup(build) {},
-    name: "rsc-fun-in-bun",
-    SECRET_SERVER_COMPONENTS_INTERNALS: {
-      router,
-      directive: {
-        client: [""],
-        server: [""],
-      },
-    },
-  }
+  const serverComponentsPlugin = ServerComponentsPlugin({
+    router,
+    server: ["./HomePage.tsx"],
+    client: ["./HomePage.tsx"],
+  })
 
-  const result = await build({
+  const result = await Bun.build({
     entrypoints: [await import.meta.resolve("../examples/HomePage.tsx")],
     target: "bun",
     outdir: "./build",
