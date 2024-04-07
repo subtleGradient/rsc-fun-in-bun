92309c6dcf2637756d9429c4cdc716bc1e88909a
Tom Aylott <oblivious@subtlegradient.com>
2024-04-06 21:33:49 -0400

    Add routes logging to server startup in rsc-fun-in-bun.ts

--- rsc-fun-in-bun.ts
+++ rsc-fun-in-bun.ts
@@ -2,7 +2,15 @@ import { verifyReactServer } from "./toy-framework/server/verify-react-server.ts
 
 if (import.meta.main) {
   verifyReactServer()
-  const { fetch } = await import("./toy-framework/server/toy-framework.server.tsx")
+  const { routes, fetch } = await import("./toy-framework/server/toy-framework.server.tsx")
   const server = Bun.serve({ fetch })
+
   console.log("Server running at", server.url.href)
+  console.log(
+    "    Responds to these routes:\n",
+    Object.keys(routes)
+      .map(pathname => `\t${server.url.origin}${pathname}`)
+      .join("\n"),
+  )
+  console.log()
 }
