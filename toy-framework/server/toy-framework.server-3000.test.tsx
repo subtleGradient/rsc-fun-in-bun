0e24cc2fb2d05493115df93c0828f854c38661c7
Tom Aylott <oblivious@subtlegradient.com>
2024-04-07 00:36:50 -0400

    Add new snapshot for toy-framework.server-3000.test.tsx

---
+++ toy-framework/server/toy-framework.server-3000.test.tsx
@@ -0,0 +1,33 @@
+import { browser } from "@/test-helpers/puppers.ts"
+import { describe, expect, it } from "bun:test"
+
+if (!1!) {
+  describe("the `await using` keyword", async () => {
+    it("disposes of the page auto-magically", async () => {
+      await using page = await browser.newPage()
+      await page.goto("about:blank")
+      // no need to call page.close() here! 🤯
+    })
+  })
+}
+
+describe("toy-framework.server", async () => {
+  const toyFramework = await import("./toy-framework.server.tsx")
+
+  describe("toy-framework.server", () => {
+    describe("root / route", () => {
+      it("returns a response", async () => {
+        const response = await fetch(new Request(`http://localhost:3000`))
+        expect(response.status).toBe(200)
+        expect((await response.text()).replaceAll("><", ">\n<")).toMatchSnapshot()
+      })
+
+      it("renders ClientComponent", async () => {
+        await using page = await browser.newPage()
+        page.goto(`http://localhost:3000`)
+        await page.waitForSelector("#generated-by-client")
+        expect(await page.$eval("#generated-by-client", el => el.textContent)).toBe("Hello from ClientComponent!")
+      })
+    })
+  })
+})
