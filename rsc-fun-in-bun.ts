fed8f3e7c31d4205e31bbb1fda8cd7159d787707
Tom Aylott <oblivious@subtlegradient.com>
2024-04-06 21:00:07 -0400

    Refactor import paths and update HTML structure in demo-RSC.server.tsx

--- rsc-fun-in-bun.ts
+++ rsc-fun-in-bun.ts
@@ -1,12 +1,7 @@
 import { verifyReactServer } from "./toy-framework/verify-react-server.ts"
 
-console.log("Hello via Bun!")
-console.debug("executing", import.meta.url)
-console.debug(process.argv)
-
-verifyReactServer()
-
 if (import.meta.main) {
+  verifyReactServer()
   const { fetch } = await import("./toy-framework/toy-framework.server.tsx")
   const server = Bun.serve({ fetch })
   console.log("Server running at", server.url.href)
