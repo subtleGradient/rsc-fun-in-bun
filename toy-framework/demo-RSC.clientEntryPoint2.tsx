50f91fea7bfa390fdf4006fcab09ad2132b0760d
Tom Aylott <oblivious@subtlegradient.com>
2024-04-06 20:52:33 -0400

    Refactor ClientComponent in demo-RSC.clientEntryPoint2.tsx

--- toy-framework/demo-RSC.clientEntryPoint2.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react"
-
-export default function ClientComponent() {
-  useEffect(() => {
-    console.log("Hello from ClientComponent!")
-  })
-  return <div id="generated-by-client">Hello from ClientComponent!</div>
-}
